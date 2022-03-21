9b536996185f3fb3c4792ae148b79ec0737a4f2d
tuliopaim <tulio.p@lyncas.net>
2022-03-21 20:28:45 -0300

    Creates a GetPeopleQueryInputValidator and NewPersonCommandResult

--- sample/EasyCqrs.Sample/Application/Commands/NewPersonCommand/NewPersonCommandResult.cs
+++ sample/EasyCqrs.Sample/Application/Commands/NewPersonCommand/NewPersonCommandResult.cs
@@ -4,6 +4,9 @@ namespace EasyCqrs.Sample.Application.Commands.NewPersonCommand;
 
 public class NewPersonCommandResult : CommandResult
 {
+    /* To validation and exception works properly */
+    public NewPersonCommandResult() {}
+
     public NewPersonCommandResult(Guid newPersonId)
     {
         NewPersonId = newPersonId;
---
+++ sample/EasyCqrs.Sample/Application/Queries/GetPeopleQuery/GetPeopleQueryInputValidator.cs
@@ -0,0 +1,13 @@
+﻿using EasyCqrs.Queries;
+using FluentValidation;
+
+namespace EasyCqrs.Sample.Application.Queries.GetPeopleQuery;
+
+public class GetPeopleQueryInputValidator : QueryInputValidator<GetPeopleQueryInput>
+{
+    public GetPeopleQueryInputValidator()
+    {
+        RuleFor(x => x.PageNumber).GreaterThanOrEqualTo(0);
+        RuleFor(x => x.PageSize).InclusiveBetween(1, 50);
+    }
+}
